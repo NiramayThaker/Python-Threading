293c92dd8708ca786b09429ee7e5ee709b930e5a
Niramay Thaker <70151582+NiramayThaker@users.noreply.github.com>
2021-11-25 06:19:03 +0530

    Using 2 threads for making a diamond pattern

--- diamond_pattern_multithreading.py
+++ diamond_pattern_multithreading.py
@@ -4,25 +4,19 @@ import threading
 
 def upper_tri():
 	for i in range(0, 5):
-
 		for k in range(0, 5 - i):
 			print('', end=' ')
-
 		for j in range(0, i):
 			print('*', end=' ')
-
 		print()
 
 
 def lower_tri():
 	for i in range(5, 0, -1):
-
 		for k in range(5 - i, 0, -1):
 			print(' ', end='')
-
 		for j in range(0, i):
 			print('*', end=' ')
-
 		print()
 
 
